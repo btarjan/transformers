3b5fdcd1c6d25decbca1b966a9ed649006f149a8
tarjanb <tarjan.b@gmail.com>
2020-04-01 23:24:41 +0200

    update lm generating scripts

---
+++ examples/run_generation.sh
@@ -0,0 +1,21 @@
+
+python ./run_generation.py \
+    --model_type=gpt2 \
+    --length=256 \
+    --prompt="jó" \
+    --num_return_sequences 180 \
+    --model_name_or_path=models/cc-gpt2-v4
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- examples/run_language_modeling.sh
+++ examples/run_language_modeling.sh
@@ -58,13 +58,150 @@ export TEST_FILE=datasets/cc/cc_dev_6-0_no-symbols.txt
 
 
 ##Train GPT-2 with gpt2(small) config on CC data
-#################################################
+################################################
 
 export TRAIN_FILE=datasets/cc/cc_train_6-0_no-symbols.txt
 export TEST_FILE=datasets/cc/cc_dev_6-0_no-symbols.txt
 
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v1 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 50 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 4 \
+#    --evaluate_during_training \
+#    --seed 42
+#    --overwrite_output_dir \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v2 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 20 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 4 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+#
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v3 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 20 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 4 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v4 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 10 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 4 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v6 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-reg_05 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 20 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 4 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-medium-v1 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-medium \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 10 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 2 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v8 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 20 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 8 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-small-v1 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-small \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 10 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 8 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
 python run_language_modeling.py \
-    --output_dir ./models/cc-gpt2-v1 \
+    --output_dir ./models/cc-gpt2-v9 \
     --model_type gpt2 \
     --config_name ./models/gpt2 \
     --tokenizer_name ./models/cc-ByteLevelBPE-nospec \
@@ -72,26 +209,12 @@ python run_language_modeling.py \
     --train_data_file=$TRAIN_FILE \
     --do_eval \
     --eval_data_file=$TEST_FILE \
-    --num_train_epochs 50 \
+    --num_train_epochs 10 \
     --save_total_limit 2 \
     --save_steps 2000 \
-    --per_gpu_train_batch_size 4 \
+    --per_gpu_train_batch_size 8 \
     --evaluate_during_training \
-    --seed 42
-#    --overwrite_output_dir \
-#    --learning_rate 1e-4
+    --seed 42 \
+    --learning_rate 1e-4
 
-##PPL Eval
-#export TEST_EVAL_FILE=datasets/cc/cc_eval_6-0_no-symbols.txt
-#
-#python run_language_modeling.py \
-#    --output_dir ./models/cc-roberta-v1 \
-#    --model_type roberta \
-#    --model_name_or_path=./models/cc-roberta-v1 \
-#    --mlm \
-#    --config_name ./models/cc-roberta-v1 \
-#    --tokenizer_name ./models/cc-roberta-v1 \
-#    --train_data_file=$TRAIN_FILE \
-#    --do_eval \
-#    --eval_data_file=$TEST_EVAL_FILE \
-#    --per_gpu_eval_batch_size 4
+
