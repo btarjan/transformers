acad642396790ba2a7f94d261d0bb263b669ae93
tarjanb <tarjan.b@gmail.com>
2020-04-08 18:59:45 +0200

    update run_generation with automatic prompt generation

--- examples/run_generation.sh
+++ examples/run_generation.sh
@@ -1,21 +1,19 @@
 
-python ./run_generation.py \
-    --model_type=gpt2 \
-    --length=256 \
-    --prompt="jó" \
-    --num_return_sequences 180 \
-    --model_name_or_path=models/cc-gpt2-v4
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+SAMPLED_TXT=datasets/cc/cc_train_6-0_no-symbols_no_empty_line.txt
+
+for i in {1..30000}; do 
+    ##Random int in range 1-6
+    NUM_OF_WORDS=$(( 1 + RANDOM % 6 ))
+    
+    ##Sampling prompt from the training text
+    PROMPT=$(shuf -n 1 ${SAMPLED_TXT} | cut -d ' ' -f 1-${NUM_OF_WORDS})
+    #echo $PROMPT
+    python ./run_generation.py \
+        --model_type=gpt2 \
+        --length=512 \
+        --prompt="${PROMPT}" \
+        --seed=${i} \
+        --num_return_sequences=90 \
+        --model_name_or_path=models/cc-gpt2-v4
+done
+
