8b34448bd345dd41b28738c75a99e71afad3dd8e
tarjanb <tarjan.b@gmail.com>
2020-04-11 14:05:43 +0200

    update tokenizer script with SentencePiece training

--- examples/tokenizer.py
+++ examples/tokenizer.py
@@ -1,7 +1,8 @@
 
 from pathlib import Path
 
-from transformers import AutoTokenizer
+import sentencepiece as spm
+from transformers import AutoTokenizer,XLNetTokenizer
 from tokenizers import ByteLevelBPETokenizer,SentencePieceBPETokenizer
 
 #paths = [str(x) for x in Path("./eo_data/").glob("**/*.txt")]
@@ -39,12 +40,26 @@ paths = ["datasets/cc/cc_train_6-0_no-symbols.txt"]
 ## XLnet tokenizer
 
 ## load a pretrained model
-tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
+#tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
 ## save model to directory
-tokenizer.save_pretrained("models/cc-SentencePiece-nospec")
+#tokenizer.save_pretrained("models/cc-SentencePiece-nospec")
 
 ## train custom tokenizer
-tokenizer = SentencePieceBPETokenizer()
-tokenizer.train(files=paths, vocab_size=30_000, min_frequency=2)
+#tokenizer = SentencePieceBPETokenizer()
+#tokenizer.train(files=paths, vocab_size=30_000, min_frequency=2)
 ## overwrite pretrained model with custom
-tokenizer.save("models/cc-SentencePiece-nospec")
+#tokenizer.save("models/cc-SentencePiece-nospec")
+
+## Train SentencePiece model
+spm.SentencePieceTrainer.Train('--input=datasets/cc/cc_train_6-0_no-symbols.txt --model_prefix=models/cc-SentencePiece-xlnet/spiece --character_coverage=1.0 --vocab_size=32000 --model_type=bpe')
+
+
+
+ #
+ #
+ #
+ #
+ #
+ #
+ #
+ #
