61a7f9f88b573db1b0a51cce341fe8ea721b1c40
tarjanb <tarjan.b@gmail.com>
2020-04-22 16:08:13 +0200

    add hu-wiki lm training script to repo

---
+++ examples/run_language_modeling_wiki.sh
@@ -0,0 +1,66 @@
+
+export TRAIN_FILE=datasets/wiki-hu/wiki-hu.txt
+export TEST_FILE=datasets/cc/cc_dev_6-0_no-symbols.txt
+
+BATCH_SIZE=8
+GRAD_ACC=1
+
+python run_language_modeling.py \
+    --output_dir ./models/huwiki-gpt2-BS_${BATCH_SIZE}-GA_${GRAD_ACC} \
+    --model_type gpt2 \
+    --config_name ./models/gpt2 \
+    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --eval_data_file=$TEST_FILE \
+    --num_train_epochs 5 \
+    --logging_steps 2000 \
+    --save_steps 20000 \
+    --per_gpu_train_batch_size ${BATCH_SIZE} \
+    --evaluate_during_training \
+    --eval_all_checkpoints \
+    --seed 42 \
+    --gradient_accumulation_steps ${GRAD_ACC} \
+    --learning_rate 1e-4
+
+BATCH_SIZE=1
+GRAD_ACC=4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/parl-gpt2-medium-2GPU-BS_${BATCH_SIZE}-GA_${GRAD_ACC} \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-medium \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 5 \
+#    --logging_steps 500 \
+#    --save_steps 5000 \
+#    --per_gpu_train_batch_size ${BATCH_SIZE} \
+#    --evaluate_during_training \
+#    --eval_all_checkpoints \
+#    --seed 42 \
+#    --gradient_accumulation_steps ${GRAD_ACC} \
+#    --learning_rate 1e-4
+
+#python run_language_modeling.py \
+#    --output_dir ./models/parl-gpt2-medium-v1 \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-medium \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 10 \
+#    --logging_steps 2000 \
+#    --save_steps 20000 \
+#    --per_gpu_train_batch_size 1 \
+#    --evaluate_during_training \
+#    --eval_all_checkpoints \
+#    --seed 42 \
+#    --learning_rate 1e-4
+
