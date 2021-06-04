9cace83d1bc0bb78abe1133119aa8d9bd6a2f511
tarjanb <tarjan.b@gmail.com>
2021-06-04 16:05:07 +0200

    add rerun config to lm training scripts

--- examples/run_language_modeling.sh
+++ examples/run_language_modeling.sh
@@ -115,25 +115,25 @@ export TEST_FILE=datasets/cc/cc_dev_6-0_no-symbols.txt
 #    --seed 42 \
 #    --learning_rate 1e-4
 
-for i in 1 4 16 64 256; do
-python run_language_modeling.py \
-    --output_dir ./models/cc-gpt2-v4-grad_acc_${i} \
-    --model_type gpt2 \
-    --config_name ./models/gpt2 \
-    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
-    --do_train \
-    --train_data_file=$TRAIN_FILE \
-    --do_eval \
-    --eval_data_file=$TEST_FILE \
-    --num_train_epochs 10 \
-    --save_total_limit 2 \
-    --save_steps 2000 \
-    --per_gpu_train_batch_size 2 \
-    --evaluate_during_training \
-    --seed 42 \
-    --learning_rate 1e-4 \
-    --gradient_accumulation_steps ${i}
-done
+#for i in 1 4 16 64 256; do
+#python run_language_modeling.py \
+#    --output_dir ./models/cc-gpt2-v4-grad_acc_${i} \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2 \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 10 \
+#    --save_total_limit 2 \
+#    --save_steps 2000 \
+#    --per_gpu_train_batch_size 2 \
+#    --evaluate_during_training \
+#    --seed 42 \
+#    --learning_rate 1e-4 \
+#    --gradient_accumulation_steps ${i}
+#done
 
 #python run_language_modeling.py \
 #    --output_dir ./models/cc-gpt2-v6 \
@@ -261,3 +261,23 @@ done
 #    --learning_rate 1e-4
 
 
+## Reconstruction of old results
+
+python run_language_modeling.py \
+    --output_dir ./models/cc-gpt2-v4_rerun \
+    --model_type gpt2 \
+    --config_name ./models/gpt2 \
+    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --eval_data_file=$TEST_FILE \
+    --num_train_epochs 10 \
+    --save_total_limit 2 \
+    --save_steps 2000 \
+    --per_gpu_train_batch_size 4 \
+    --evaluate_during_training \
+    --seed 42 \
+    --learning_rate 1e-4 \
+    --gradient_accumulation_steps 1 \
+    --overwrite_output_dir \
--- examples/run_language_modeling_parl.sh
+++ examples/run_language_modeling_parl.sh
@@ -34,24 +34,24 @@ GRAD_ACC=4
 BATCH_SIZE=1
 GRAD_ACC=4
 
-python run_language_modeling.py \
-    --output_dir ./models/parl-gpt2-medium-2GPU-BS_${BATCH_SIZE}-GA_${GRAD_ACC} \
-    --model_type gpt2 \
-    --config_name ./models/gpt2-medium \
-    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
-    --do_train \
-    --train_data_file=$TRAIN_FILE \
-    --do_eval \
-    --eval_data_file=$TEST_FILE \
-    --num_train_epochs 5 \
-    --logging_steps 500 \
-    --save_steps 5000 \
-    --per_gpu_train_batch_size ${BATCH_SIZE} \
-    --evaluate_during_training \
-    --eval_all_checkpoints \
-    --seed 42 \
-    --gradient_accumulation_steps ${GRAD_ACC} \
-    --learning_rate 1e-4
+#python run_language_modeling.py \
+#    --output_dir ./models/parl-gpt2-medium-2GPU-BS_${BATCH_SIZE}-GA_${GRAD_ACC} \
+#    --model_type gpt2 \
+#    --config_name ./models/gpt2-medium \
+#    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+#    --do_train \
+#    --train_data_file=$TRAIN_FILE \
+#    --do_eval \
+#    --eval_data_file=$TEST_FILE \
+#    --num_train_epochs 5 \
+#    --logging_steps 500 \
+#    --save_steps 5000 \
+#    --per_gpu_train_batch_size ${BATCH_SIZE} \
+#    --evaluate_during_training \
+#    --eval_all_checkpoints \
+#    --seed 42 \
+#    --gradient_accumulation_steps ${GRAD_ACC} \
+#    --learning_rate 1e-4
 
 #python run_language_modeling.py \
 #    --output_dir ./models/parl-gpt2-medium-v1 \
@@ -70,4 +70,28 @@ python run_language_modeling.py \
 #    --eval_all_checkpoints \
 #    --seed 42 \
 #    --learning_rate 1e-4
+
+## Reconstruction of old results
+
+BATCH_SIZE=1
+GRAD_ACC=1
+
+python run_language_modeling.py \
+    --output_dir ./models/parl-gpt2-BS_${BATCH_SIZE}-GA_${GRAD_ACC}_rerun \
+    --model_type gpt2 \
+    --config_name ./models/gpt2 \
+    --tokenizer_name ./models/cc-ByteLevelBPE-gpt2 \
+    --do_train \
+    --train_data_file=$TRAIN_FILE \
+    --do_eval \
+    --eval_data_file=$TEST_FILE \
+    --num_train_epochs 10 \
+    --logging_steps 500 \
+    --save_steps 5000 \
+    --per_gpu_train_batch_size ${BATCH_SIZE} \
+    --evaluate_during_training \
+    --eval_all_checkpoints \
+    --seed 42 \
+    --gradient_accumulation_steps ${GRAD_ACC} \
+    --learning_rate 1e-4
 
